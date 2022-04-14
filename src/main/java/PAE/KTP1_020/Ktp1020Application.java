8ddaf593284431d3bdac825eb1a852bada70e2a9
FatkhiAlauddin <cupetongspeed12@gmail.com>
2022-04-14 22:16:34 +0700

    first commit

---
+++ src/main/java/PAE/KTP1_020/Ktp1020Application.java
@@ -0,0 +1,13 @@
+package PAE.KTP1_020;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ktp1020Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ktp1020Application.class, args);
+	}
+
+}
---
+++ src/test/java/PAE/KTP1_020/Ktp1020ApplicationTests.java
@@ -0,0 +1,13 @@
+package PAE.KTP1_020;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ktp1020ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
