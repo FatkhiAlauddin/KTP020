4ef86b8f4719fc8ce2b79280dab40882a52d84f2
FatkhiAlauddin <cupetongspeed12@gmail.com>
2022-05-19 00:52:17 +0700

    first commit

--- src/main/java/PAE/KTP1_020/DataController.java
+++ src/main/java/PAE/KTP1_020/DataController.java
@@ -4,7 +4,10 @@
  */
 package PAE.KTP1_020;
 
+import java.util.ArrayList;
+import java.util.List;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -14,8 +17,31 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class DataController {
     
+    DataJpaController datactrl = new DataJpaController();
+    List<Data> newdata = new ArrayList<>();
+    
+    @RequestMapping("/main")
+    public String getMain(){
+        return "menu";
+    }
+    
     @RequestMapping("/data")
-    public String getDataKTP(){
+    //@ResponseBody
+    public String getDataKTP(Model model){
+        int record = datactrl.getDataCount();
+        String result="";
+        try{
+            newdata = datactrl.findDataEntities().subList(0, record);
+        }
+        catch (Exception e){result = e.getMessage();}
+        model.addAttribute("goData", newdata);
+        model.addAttribute("record", record);
+        
         return "database";
+    }
+    
+    @RequestMapping("/edit")
+    public String doEdit(){
+        return "editktp";
     }
 }
