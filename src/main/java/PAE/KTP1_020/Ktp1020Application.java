018bef8aae8e26fc85e407aa7dfd3fac92343a3b
FatkhiAlauddin <cupetongspeed12@gmail.com>
2022-06-10 12:51:41 +0700

    first commit

--- src/main/java/PAE/KTP1_020/Ktp1020Application.java
+++ src/main/java/PAE/KTP1_020/Ktp1020Application.java
@@ -2,12 +2,18 @@ package PAE.KTP1_020;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Ktp1020Application {
+public class Ktp1020Application extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Ktp1020Application.class, args);
 	}
+        @Override
+        protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(Ktp1020Application.class);
+    }
 
 }
